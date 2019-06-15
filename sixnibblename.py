a410732718d05282d592b2afd4631792b5d5a9de
Oliver Robson <oliver.robson.ecse@gmail.com>
2019-06-15 14:17:25 +1000

    Fixed ESP32 devices having the same name

--- sixnibblename.py
+++ sixnibblename.py
@@ -4,7 +4,11 @@ V = 'aeiou'
 def get(sb=None):
     if not sb:
         from machine import unique_id
-        sb = int.from_bytes(unique_id(), 'little')
+        import sys
+        uid = unique_id()
+        if sys.platform == 'esp32':
+            uid = uid[3:]
+        sb = int.from_bytes(uid, 'little')
     n = ''
     n += C[((sb & 0xFE0000) >> 17) % len(C)].upper()
     n += V[((sb & 0x1F000) >> 12) % len(V)]
